663b855603bee2ad60b85902b123d22f1775282c
EREN_2 <EREN_2@EKNB>
2022-01-19 11:30:31 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    //Business prjesine sğ tıklanıp Add > Reference Project ile
+    // DataAccess ve Entities projeleri referans gösterilir.
+    public interface IProductService
+    {
+        List<Product> GetAll();
+
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // IProductDal : I-Interface Product-Entity Dal-Layer
+    // Interface dafault public değildir, metodları publictir.
+    public interface IProductDal
+    {
+        //Product başka bir projede tanımlı olduğu için
+        //Öncelikle projeye sağ tıklanıp Add-Project Reference ile 
+        //Entities projesi seçilerek referans eklenir.
+        //Sonrasında kodda using ile Product'ın namespace'i eklenir. using Entities.Concrete
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        
+        List<Product> GetAllByCategory(int categoryId);
+
+    }
+}
