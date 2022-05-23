e48b9d5b7517a12f731870e9840acc12fdb87d10
@EKaplanSE <erenkaplan78@gmail.com>
2022-05-23 15:04:16 +0300

    230522

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,7 @@ namespace DataAccess.Abstract
         
         List<Product> GetAllByCategory(int categoryId);
         */
-
+        List<ProductDetailDto> GetProductDetails();
     }
 }
 
